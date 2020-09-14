8137587b485bf4c584470a3920d5ee726ae808b8
lichen105519 <lichen105519@pwrd.com>
2020-09-14 20:37:25 +0800

    解决切换路由闪屏bug

--- src/test/hooks/router/index.jsx
+++ src/test/hooks/router/index.jsx
@@ -1,14 +1,19 @@
 import dynamicComponent from 'dynamic-component';
 import usePromise from 'use-promise';
 import { Route, Redirect, withRouter} from 'react-router-dom';
-import React,{useMemo} from 'react';
+import React,{useMemo,useRef} from 'react';
 
 export default (promise,props) => {
     let result = usePromise(promise);
+    let Page = useRef({});
     let component = useMemo(() => {
         if(!result){ return null }
         let defaultPage = null;
-        let curPage = null;
+        //判断当前页面是否为需要页面
+        if(Page.current.pathname != props.location.pathname){
+            Page.current = null;
+        }
+        //遍历
         Object.keys(result.default).every(key => {
             let item = result.default[key];
             let pageKey = `/${key}`;
@@ -17,21 +22,18 @@ export default (promise,props) => {
                 defaultPage = <Route path='/' exact render={ r => <Redirect to = {pageKey}/>}/>;
             };
             //判断当前页面
-            if(props.location.pathname == pageKey){
-                curPage = <Route 
-                    key={key} 
-                    exact = {true} 
-                    strict = {true}
-                    path = {'/' + key} 
-                    component = { dynamicComponent(import(`../../pages/${item.path}`),props) }
-                />;
+            if(!Page.current && props.location.pathname == pageKey){
+                Page.current = {
+                    pathname : props.location.pathname,
+                    component : dynamicComponent(import(`../../pages/${item.path}`))
+                }
             }
             //判断是否继续
-            return !defaultPage || !curPage;
+            return !defaultPage || !Page.current;
         });
         return <>
             {defaultPage}
-            {curPage}
+            {Page ? <Page.current.component {...props}/> : Page}
         </>
     },[result,props]);
 
